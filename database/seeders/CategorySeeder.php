6cd0523f029673e80f3b7773dc88fd8d8a5d73ad
Lotrando <miroslav.klika@seznam.cz>
2023-03-13 06:30:20 +0100

    bozp update v praci

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -402,7 +402,7 @@ class CategorySeeder extends Seeder
             'category_name' => 'Provozně bezp. předpisy',
             'folder_name'   => 'provozne-bezpecnostni-predpisy',
             'category_icon' => 'provozne-bezpecnostni-predpisy.png',
-            'svg_icon'      => '<svg xmlns="http://www.w3.org/2000/svg" class="icon text-teal" width="40" height="40" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M11.99 1.968c1.023 0 1.97 .521 2.512 1.359l.103 .172l7.1 12.25l.062 .126a3 3 0 0 1 -2.568 4.117l-.199 .008h-14l-.049 -.003l-.112 .002a3 3 0 0 1 -2.268 -1.226l-.109 -.16a3 3 0 0 1 -.32 -2.545l.072 -.194l.06 -.125l7.092 -12.233a3 3 0 0 1 2.625 -1.548zm.02 12.032l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -6a1 1 0 0 0 -.993 .883l-.007 .117v2l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-2l-.007 -.117a1 1 0 0 0 -.993 -.883z" stroke-width="0" fill="currentColor"></path></svg>',
+            'svg_icon'      => '<svg xmlns="http://www.w3.org/2000/svg" class="icon text-teal" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path: stroke="none" d="M0 0h24v24H0z" fill="none"></path:<path d="M3 5a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-14z"></path><path d="M12 8v4"></path><path d="M12 16h.01"></path></svg>',
             'fa_icon'      => null,
             'button'        => 'provozně bezp. předpisy',
             'color'         => 'teal'
